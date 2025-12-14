b321594cf7e43cc78fea2553760a1c645238bc6c
TheBedanse <05832012@mail.ru>
2025-12-14 04:37:50 +0300

    edit L1.10

--- L1.10/main.go
+++ L1.10/main.go
@@ -1,27 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func groupTemperatures(temps []float64) map[int][]float64 {
+	groups := make(map[int][]float64)
+
+	for _, temp := range temps {
+
+		var key int
+		if temp >= 0 {
+			key = int(math.Floor(temp/10)) * 10
+		} else {
+			key = int(math.Ceil(temp/10)) * 10
+		}
+
+		groups[key] = append(groups[key], temp)
+	}
+
+	return groups
+}
 
 func main() {
-	temp := [8]float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
-	var groupMinusTwenty []float64
-	var groupTen []float64
-	var groupTwenty []float64
-	var groupThirty []float64
+	temperatures := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
 
-	for _, i := range temp {
-		if i <= -20 && i > -30 {
-			groupMinusTwenty = append(groupMinusTwenty, i)
-		} else if i < 20 && i >= 10 {
-			groupTen = append(groupTen, i)
-		} else if i < 30 && i >= 20 {
-			groupTwenty = append(groupTwenty, i)
-		} else if i < 40 && i >= 30 {
-			groupThirty = append(groupThirty, i)
-		}
+	groups := groupTemperatures(temperatures)
+
+	for key, values := range groups {
+		fmt.Printf("%d: %v\n", key, values)
 	}
-	fmt.Printf("-20:%v\n", groupMinusTwenty)
-	fmt.Printf("10:%v\n", groupTen)
-	fmt.Printf("20:%v\n", groupTwenty)
-	fmt.Printf("30:%v\n", groupThirty)
 }
